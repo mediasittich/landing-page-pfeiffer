c001f05deaf83c68265c3da2d96f765b1920a12b
Regina Galambos <gina.galambos@gmail.com>
2019-03-13 23:28:00 +0100

    add dev-server, html content & npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+        port: 3000,
+        open: true,
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
