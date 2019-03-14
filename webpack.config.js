4a9b6d7109982bf46038c667bd8ea03e26764e1e
Regina Galambos <gina.galambos@gmail.com>
2019-03-14 01:00:26 +0100

    setup html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -9,8 +10,26 @@ module.exports = {
         port: 3000,
         open: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Pfeiffer & Partner',
+            template: 'dist/index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
+            }
+        })
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, 'src')
+        ],
+        extensions: ['.js'],
     }
 }
