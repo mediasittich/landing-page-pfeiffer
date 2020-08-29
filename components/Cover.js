b42664de854caa60f0f582ee750d178d920b9d9a
Regina Galambos <gina.galambos@gmail.com>
2020-08-29 23:48:22 +0200

    rm react stuff

--- components/Cover.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Cover = () => {
-  return <div>Cover </div>;
-};
-
-export default Cover;
--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const index = () => {
-  return <div> Hello Contentful </div>;
-};
-
-export default index;
